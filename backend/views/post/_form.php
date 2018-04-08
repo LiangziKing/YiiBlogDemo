8cb8b2e74798b51c2ba5d63e6e8043d47c8510a1
LiangziKing <1990818918@qq.com>
2018-04-08 17:02:40 +0800

    完成文章修改和新增功能

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -2,6 +2,10 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\Poststatus;
+use yii\helpers\ArrayHelper;
+use common\models\common\models;
+use common\models\Adminuser;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
@@ -18,16 +22,55 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?php
+        /*
+        第一种方法：
+        $psObjs = Poststatus::find()->all();
+        $allStatus = ArrayHelper::map($psObjs,'id','name');
+
+        第二种方法：
+        $psArray = Yii::$app->db->createCommand('select id,name from poststatus')->queryAll();
+        $allStatus = ArrayHelper::map($psArray,'id','name');
+
+        第三种方法：
+        $allStatus = (new \yii\db\Query())
+        ->select(['name','id'])
+        ->from('poststatus')
+        ->indexBy('id')
+        ->column();
+
+        第四种方法：
+        allStatus = Poststatus::find()
+        ->select(['name','id'])
+        ->orderBy('position')
+        ->indexBy('id')
+        ->column();
+        */
+    ?>
+
+    <?= $form->field($model, 'status')
+        ->dropDownList(Poststatus::find() 
+        ->select(['name','id'])
+        ->orderBy('position')
+        ->indexBy('id')
+        ->column(),
+        ['prompt'=>'请选择状态']);
+    ?>
 
     <?= $form->field($model, 'create_time')->textInput() ?>
 
     <?= $form->field($model, 'update_time')->textInput() ?>
 
-    <?= $form->field($model, 'author_id')->textInput() ?>
+    <?= $form->field($model, 'author_id')
+        ->dropDownList(Adminuser::find()
+        ->select(['nickname','id'])
+        ->indexBy('id')
+        ->column(),
+        ['prompt'=>'请选择状态']); 
+    ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton($model->isNewRecord ? '新增' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
